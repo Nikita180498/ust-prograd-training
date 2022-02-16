e940e62913fd7bb7dba2e3586ac8a14635277a13
Nikita Nansure <nikinansure1818@gmail.com>
2022-02-16 16:27:12 +0530

    update

--- BabyStore/src/main/java/com/example/BabyStore/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.BabyStore.repository;
-
-import com.example.BabyStore.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category,Integer> {
-}
--- BabyStore/src/main/java/com/example/BabyStore/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.BabyStore.repository;
-
-import com.example.BabyStore.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
-
-public interface UserRepository extends JpaRepository<User,String> {
-}
